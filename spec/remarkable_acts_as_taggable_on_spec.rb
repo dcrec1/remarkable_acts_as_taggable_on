75cffb60d87c9b81893fe4b12783e2f7674df918
Diego Carrion <dc.rec1@gmail.com>
2009-05-06 11:05:44 -0300

    fixed spec stubs

--- spec/remarkable_acts_as_taggable_on_spec.rb
+++ spec/remarkable_acts_as_taggable_on_spec.rb
@@ -5,20 +5,20 @@ end
 
 describe Remarkable::Acts::TaggableOn do
   it "should confirm a model is acting as taggable on an attribute" do
-    Person.stub!(:categories)
-    Person.stub!(:category_list)
-    act_as_taggable_on(:categories).matches?(Person).should be_true
+    person = mock(Person, :categories => false,
+                          :category_list => false)
+    act_as_taggable_on(:categories).matches?(person).should be_true
   end
   
   it "should confirm a model is not acting as taggable on an attribute" do
-    act_as_taggable_on(:members).matches?(Person).should be_false
+    act_as_taggable_on(:members).matches?(mock(Person)).should be_false
   end
   
   it "should confirm multiple tagging attributes at once" do
-    Person.stub!(:categories)
-    Person.stub!(:category_list)
-    Person.stub!(:tags)
-    Person.stub!(:tag_list)
-    act_as_taggable_on(:categories, :tags).matches?(Person).should be_true
+    person = mock(Person, :categories => false,
+                          :category_list => false,
+                          :tags => false,
+                          :tag_list => false)
+    act_as_taggable_on(:categories, :tags).matches?(person).should be_true
   end
 end
