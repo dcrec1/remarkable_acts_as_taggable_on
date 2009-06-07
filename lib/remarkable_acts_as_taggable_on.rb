91f0b922c2cc9fe62ad3be217b02a2348e5494f1
Diego Carrion <dc.rec1@gmail.com>
2009-06-07 15:48:06 -0300

    implemented acts_as_tagger

--- lib/remarkable_acts_as_taggable_on.rb
+++ lib/remarkable_acts_as_taggable_on.rb
@@ -12,9 +12,23 @@ module Remarkable
               @subject.respond_to? @attribute and @subject.respond_to? "#{@attribute.to_s.singularize}_list"
             end
         end
+        
+        class ActAsTaggerMatcher < Remarkable::ActiveRecord::Base
+          assertion :is_tagger?
+          protected
+          
+            def is_tagger?
+              klass = @subject.class
+              klass.respond_to? :is_tagger? and klass.is_tagger?
+            end
+        end
 
         def act_as_taggable_on(*args)
           ActAsTaggableOnMatcher.new(*args).spec(self)
+        end
+        
+        def act_as_tagger
+          ActAsTaggerMatcher.new.spec(self)
         end
       end
     end
--- spec/remarkable_acts_as_taggable_on_spec.rb
+++ spec/remarkable_acts_as_taggable_on_spec.rb
@@ -1,24 +1,39 @@
 require File.join(File.dirname(__FILE__), "spec_helper.rb")
 
-class Person < ActiveRecord::Base
+class Person
+  def self.is_tagger?; true; end
+end
+
+class User
+  def categories; end;
+  def category_list; end;
+  def tags; end;
+  def tag_list; end;
 end
 
 describe Remarkable::Acts::TaggableOn do
+  before :each do
+    @person = Person.new
+    @user = User.new
+  end
+  
   it "should confirm a model is acting as taggable on an attribute" do
-    person = mock(Person, :categories => false,
-                          :category_list => false)
-    act_as_taggable_on(:categories).matches?(person).should be_true
+    act_as_taggable_on(:categories).matches?(@user).should be_true
   end
   
   it "should confirm a model is not acting as taggable on an attribute" do
-    act_as_taggable_on(:members).matches?(mock(Person)).should be_false
+    act_as_taggable_on(:members).matches?(@person).should be_false
   end
   
   it "should confirm multiple tagging attributes at once" do
-    person = mock(Person, :categories => false,
-                          :category_list => false,
-                          :tags => false,
-                          :tag_list => false)
-    act_as_taggable_on(:categories, :tags).matches?(person).should be_true
+    act_as_taggable_on(:categories, :tags).matches?(@user).should be_true
+  end
+  
+  it "should confirm a model is acting as tagger" do
+    act_as_tagger.matches?(@person).should be_true
+  end
+  
+  it "should confirm a model is not acting as tagger" do
+    act_as_tagger.matches?(@user).should be_false
   end
 end
